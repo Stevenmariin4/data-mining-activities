c386af41f4a6c8362b504d23f8ea0a84332d9a1e
Jonnathan Arevalo <jarevalo@robotec.com>
2022-10-04 21:41:47 -0500

    validate method kmeans

--- k-medias.R
+++ k-medias.R
@@ -1,4 +1,5 @@
 database <- read.csv('~/Desktop/Universidad/Mineria de datos/data-mining-activities/Market.csv', sep=',', header = TRUE)
 grupok<-kmeans(database[,9:10],center=4)
-table(grupok$cluster,datos_food[,13])
+grupok
+table(grupok$cluster,datos_food[,3])
 plot(grupok$cluster)
