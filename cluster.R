c74b9a5d3114286367200d6e2a4023950d7b89a1
Jonnathan Arevalo <jarevalo@robotec.com>
2022-10-05 20:27:42 -0500

    modify database and adapter model with new databse

--- cluster.R
+++ cluster.R
@@ -1,5 +1,5 @@
 database <- read.csv('~/Desktop/Universidad/Mineria de datos/data-mining-activities/Market.csv', sep=',', header = TRUE)
-summary(datos_food)
+summary(database)
 database[,7:9]<-scale(database[,7:9])
 distancia_euclidiana<-dist(database[,7:9])
 agrupamiento<-hclust(distancia_euclidiana)
@@ -7,6 +7,8 @@ agrupamiento<-hclust(distancia_euclidiana)
 
 grupos<-cutree(agrupamiento,k=1)
 
-plot(agrupamiento,hand=1,cex=0.8,labels=datos_food[,5],
+par(mar= c(1,1,1,1))
+plot(1:30)
+plot(agrupamiento,hand=1,cex=0.8,labels=database[,6],
      main="analisis de cluster")
 rect.hclust(agrupamiento,k=25,border="red")
--- k-medias.R
+++ k-medias.R
@@ -1,5 +1,5 @@
 database <- read.csv('~/Desktop/Universidad/Mineria de datos/data-mining-activities/Market.csv', sep=',', header = TRUE)
-grupok<-kmeans(database[,9:10],center=4)
+grupok<-kmeans(database[,7:9],center=4)
 grupok
-table(grupok$cluster,datos_food[,3])
+table(grupok$cluster,database[,6])
 plot(grupok$cluster)
